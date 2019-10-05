9b92251576f2bacfe2854a5a58b640158244bac9
Delirios <lushchevskiyd@gmail.com>
2019-10-05 16:03:44 +0300

    Last version

--- Lab_1/Lab_1/Form1.Designer.cs
+++ Lab_1/Lab_1/Form1.Designer.cs
@@ -36,6 +36,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
@@ -97,6 +103,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(237, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -109,26 +116,83 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(374, 340);
+            this.textBox2.Location = new System.Drawing.Point(12, 406);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.Size = new System.Drawing.Size(238, 20);
             this.textBox2.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(608, 340);
+            this.button1.Location = new System.Drawing.Point(632, 338);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 23);
             this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
+            this.button1.Text = "Зашифрувати";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(377, 406);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(238, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(377, 342);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(238, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Зашифрований текст";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Розшифрувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Розшифрований текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Введіть зашифрований текст";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -159,6 +223,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Lab_1/Lab_1/Form1.cs
+++ Lab_1/Lab_1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,48 +13,44 @@ namespace Lab_1
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-
-            char[,] datagrid_1 = new char[,] {
+        char[,] datagrid_1 = new char[,] {
                 { 'Q', 'A', 'Z', 'W', 'S' },
                 { 'X', 'C', 'D', 'E', 'R' },
                 { 'F', 'V', 'B', 'G', 'T' },
                 { 'Y', 'H', 'N', 'M', 'U' },
                 { 'I', 'K', 'L', 'O', 'P' },
             };
-            char[,] datagrid_2 = new char[,] {
+        char[,] datagrid_2 = new char[,] {
                 { 'P', 'L', 'M', 'K', 'O' },
                 { 'I', 'N', 'U', 'H', 'B' },
                 { 'Y', 'G', 'V', 'T', 'F' },
                 { 'C', 'X', 'D', 'R', 'E' },
                 { 'W', 'S', 'Q', 'A', 'Z' },
             };
-            char[,] datagrid_3 = new char[,] {
+        char[,] datagrid_3 = new char[,] {
                 { 'A', 'Q', 'B', 'L', 'Z' },
                 { 'S', 'D', 'C', 'F', 'X' },
                 { 'P', 'V', 'W', 'R', 'N' },
                 { 'M', 'Y', 'T', 'E', 'G' },
                 { 'I', 'U', 'O', 'K', 'H' },
             };
-            char[,] datagrid_4 = new char[,] {
+        char[,] datagrid_4 = new char[,] {
                 { 'Q', 'W', 'E', 'R', 'T' },
                 { 'A', 'S', 'D', 'F', 'Z' },
                 { 'P', 'O', 'I', 'U', 'Y' },
                 { 'L', 'K', 'H', 'M', 'N' },
                 { 'X', 'C', 'V', 'B', 'G' },
             };
+        public Form1()
+        {
+            InitializeComponent();
+
+            
 
             PutArrayInDGV(datagrid_1,1);
             PutArrayInDGV(datagrid_2,2);
             PutArrayInDGV(datagrid_3,3);
             PutArrayInDGV(datagrid_4,4);
-
-
-            //string text = textBox1.Text;
-            //char[] textboxchar = text.ToCharArray();
-            //List<string> list = SplitString(text);
 
         }
 
@@ -62,18 +59,7 @@ namespace Lab_1
             
         }
 
-        static List<string> SplitString(string str)
-        {
-            List<string> list = new List<string>();
-            int i = 0;
-            while (i < str.Length - 1)
-            {
-                list.Add(str.Substring(i, 2));
-                i += 2;
-            }
-            return list;
-        }
-
+        #region Creating an array
         void PutArrayInDGV(char[,] array, int number)
         {
             DataGridView[] data = new DataGridView[] { dataGridView1, dataGridView2, dataGridView3, dataGridView4 };
@@ -92,7 +78,7 @@ namespace Lab_1
                 }
             }          
         }
-
+        #endregion
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
@@ -100,13 +86,214 @@ namespace Lab_1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            textBox4.Clear();
+            encryption();
+        }
+        #region Encryption. 
+
+
+        public void encryption()
+        {
+            string letters_2 = "";
+            string letters_1 = "";
             string text = textBox1.Text;
-            //char[] textboxchar = text.ToCharArray();
-            List<string> list = list.Add(text);
-            for (int i = 0; i < list.Count; i++)
+            text = text.Replace(" ", "");
+            char[] arr = text.ToUpper().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
             {
-                textBox2.Lines[i] = list[i].ToString() + "\r\n";
+                if (i % 2 == 0)
+                {
+                    for (int x = 0; x < datagrid_1.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < datagrid_1.GetLength(1); y++)
+                        {
+                            if (arr[i] == datagrid_1[x, y])
+                            {
+                                string coordinate_x = x.ToString();
+                                string coordinate_y = y.ToString();
+
+                                letters_1 += coordinate_x + " " + coordinate_y + " ";
+
+                            }
+                        }
+                    }
+                }
+                if (i % 2 != 0)
+                {
+                    for (int x = 0; x < datagrid_4.GetLength(0); x++)
+                    {
+
+                        for (int y = 0; y < datagrid_4.GetLength(1); y++)
+                        {
+                            if (arr[i] == datagrid_4[x, y])
+                            {
+                                string coordinate_x = x.ToString();
+                                string coordinate_y = y.ToString();
+
+                                letters_2 += coordinate_x + " " + coordinate_y + " ";
+                            }
+                        }
+                    }
+                }
+
             }
+
+
+            String[] words_1 = letters_1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            String[] words_2 = letters_2.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //textBox4.Text = letters_1;
+            //textBox3.Text = letters_2;
+            string l = "";
+            int j = -1;
+            int s = -2;
+            int k = -1;
+            for (int i = 0; i < words_1.Length; i += 2)
+            {
+                //j += 2;
+                for (j += 2; j < words_2.Length;)
+                {
+                    l += words_1[i] + " " + words_2[j] + " ";
+
+                    for (k += 2; k < words_1.Length;)
+                    {
+                        for (s += 2; s < words_2.Length;)
+                        {
+                            l += words_2[s] + " " + words_1[k] + " ";
+                            break;
+                        }
+                        break;
+                    }
+                    break;
+                }
+
+            }
+            String[] coordinate_after = l.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int t = -2;
+            for (int i = 0; i < coordinate_after.Length; i += 4)
+            {
+
+                int n = Convert.ToInt32(coordinate_after[i]);
+                int m = Convert.ToInt32(coordinate_after[i + 1]);
+                textBox4.Text += datagrid_2[n, m];
+                for (t += 4; t < coordinate_after.Length;)
+                {
+                    int x = Convert.ToInt32(coordinate_after[t]);
+                    int y = Convert.ToInt32(coordinate_after[t + 1]);
+                    textBox4.Text += datagrid_3[x, y];
+                    break;
+                }
+            }
+        }
+        #endregion
+
+
+        #region Decryption
+        public void decryption()
+        {
+            string letters_2 = "";
+            string letters_1 = "";
+            string text = textBox2.Text;
+            char[] arr = text.ToUpper().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int x = 0; x < datagrid_2.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < datagrid_2.GetLength(1); y++)
+                        {
+                            if (arr[i] == datagrid_2[x, y])
+                            {
+                                string coordinate_x = x.ToString();
+                                string coordinate_y = y.ToString();
+
+                                letters_1 += coordinate_x + " " + coordinate_y + " ";
+
+                            }
+                        }
+                    }
+                }
+                if (i % 2 != 0)
+                {
+                    for (int x = 0; x < datagrid_3.GetLength(0); x++)
+                    {
+
+                        for (int y = 0; y < datagrid_3.GetLength(1); y++)
+                        {
+                            if (arr[i] == datagrid_3[x, y])
+                            {
+                                string coordinate_x = x.ToString();
+                                string coordinate_y = y.ToString();
+
+                                letters_2 += coordinate_x + " " + coordinate_y + " ";
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+            String[] words_1 = letters_1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            String[] words_2 = letters_2.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //textBox4.Text = letters_1;
+            //textBox3.Text = letters_2;
+            string l = "";
+            int j = -1;
+            int s = -2;
+            int k = -1;
+            for (int i = 0; i < words_1.Length; i += 2)
+            {
+                //j += 2;
+                for (j += 2; j < words_2.Length;)
+                {
+                    l += words_1[i] + " " + words_2[j] + " ";
+
+                    for (k += 2; k < words_1.Length;)
+                    {
+                        for (s += 2; s < words_2.Length;)
+                        {
+                            l += words_2[s] + " " + words_1[k] + " ";
+                            break;
+                        }
+                        break;
+                    }
+                    break;
+                }
+
+            }
+            String[] coordinate_after = l.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int t = -2;
+            for (int i = 0; i < coordinate_after.Length; i += 4)
+            {
+
+                int n = Convert.ToInt32(coordinate_after[i]);
+                int m = Convert.ToInt32(coordinate_after[i + 1]);
+                textBox3.Text += datagrid_1[n, m];
+                for (t += 4; t < coordinate_after.Length;)
+                {
+                    int x = Convert.ToInt32(coordinate_after[t]);
+                    int y = Convert.ToInt32(coordinate_after[t + 1]);
+                    textBox3.Text += datagrid_4[x, y];
+                    break;
+                }
+            }
+        }
+
+
+
+        #endregion
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            decryption();
         }
     }
 }
